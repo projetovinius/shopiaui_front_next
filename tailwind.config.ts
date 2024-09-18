1da1c3479f251c562c94b283ae20957f481e684c
BrunaCardoso7 <obitadrawing@gmail.com>
2024-09-17 23:49:48 -0300

    feat: add config tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import { Config } from 'tailwindcss'
+ 
+const config: Config = {
+    darkMode: ['class'],
+    content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}', 
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+ 
+    // Or if using `src` directory:
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
+  },
+  plugins: [require("tailwindcss-animate")],
+}
+export default config
